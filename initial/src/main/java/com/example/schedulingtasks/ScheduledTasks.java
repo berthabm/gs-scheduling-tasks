6a3482a40d743083e8ab03e957be0c24a234ba3a
berthabm <berthaburgosmendez@gmail.com>
2022-06-15 15:07:15 -0500

    Bertha Rest 1.2

--- initial/src/main/java/com/example/schedulingtasks/ScheduledTasks.java
+++ initial/src/main/java/com/example/schedulingtasks/ScheduledTasks.java
@@ -1,8 +1,11 @@
 package com.example.schedulingtasks;
 
+import static org.mockito.ArgumentMatchers.any;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -80,48 +83,101 @@ public class ScheduledTasks {
        // List<Object> resultadoId = (List<Object>)tPrueba.donde("asistencia = 0").seleccionar("id");
 
         List<Object> resultadoaTotalizadorH = (List<Object>) aTotalizadorH.seleccionar("*");
-        List<Object> resultadoaVenta = (List<Object>) aTotalizadorH.seleccionar("*");
+        List<Object> resultadoaVenta = (List<Object>) aVenta.ordenadoPor("\"veID\"").seleccionar("*");
+        List<Object> ejemploLista = new ArrayList<Object>();
         
+        for(int i = 0; i < resultadoaVenta.size(); i++) {
+        	Map<String, Object> itemVenta = (Map<String, Object>) resultadoaVenta.get(i);
+        	
+        	Object horaVe = itemVenta.get("veHoraPC");
+        	Object caraVe = itemVenta.get("veCara");
+        	Object estadoVe = itemVenta.get("veEstado");
+        	
+        	for(int j = 0; j < resultadoaTotalizadorH.size(); j++) {
+            	Map<String, Object> itemTotalizador = (Map<String, Object>) resultadoaTotalizadorH.get(j);
+            	
+            	Object horaTh = itemTotalizador.get("thHora");
+            	Object caraTh = itemTotalizador.get("thCara");
+            	Object importeTh = itemTotalizador.get("thImporte");
+            	Object volumenTh = itemTotalizador.get("thVolumen");
+            	
+            	if(horaTh.equals(horaVe)&& caraVe.equals(caraTh)&&estadoVe.equals(0)) {
+            		aVenta.donde("\"veEstado\"=0");
+            		
+            		ejemploLista.add(importeTh);
+            		ejemploLista.add(volumenTh);
+            	} else {
+            		
+            	}
+        	}
+        	
+        }
         
-        for(int i = 0; i < resultadoaTotalizadorH.size(); i++) {
-        	Map<String, Object> item = (Map<String, Object>) resultadoaTotalizadorH.get(i);
-        	//Map<String, Object> item2 = (Map<String, Object>) resultadoId.get(i);
+
+        List<Object> resultadoQuery = (List<Object>)aVenta.seleccionar("*");
+        for(int i = 0; i < resultadoQuery.size(); i++) {
+        	Map<String, Object> itemQuery = (Map<String, Object>) resultadoQuery.get(i);
+        	Object importeTh = ejemploLista.get(i);
+        	Object volumenTh = ejemploLista.get(i + 1);
+        
+        	SimpleDateFormat SDFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         	
-        	System.out.println(item);
+        	System.out.print(itemQuery.get("veID") + " ");
         	
-        	System.out.println(item.get("thID"));
+            Date fecha = (Date) itemQuery.get("veHoraPC");      	
+        	String veHoraPC = SDFormat.format(fecha);
         	
-        	System.out.println(item.get("thHora"));
-        	System.out.println(item.get("thCara"));
-        	System.out.println(item.get("thManguera"));
-        	System.out.println(item.get("thTipo"));
-        	System.out.println(item.get("thImporte"));
-        	System.out.println(item.get("thVolumen"));
+        	System.out.print(veHoraPC + " ");
         	
-        	/*Date fecha = (Date) item.get("fechanacimiento");
+        	fecha = (Date) itemQuery.get("veHoraCS"); 
+        	String veHoraCS = SDFormat.format(fecha);
         	
-        	SimpleDateFormat SDFormat = new SimpleDateFormat("dd/MM/yyyy");
-        	String fechaCast = SDFormat.format(fecha);
+        	System.out.print(veHoraCS + " ");
+        	System.out.print(itemQuery.get("veCara")+ " ");
+        	System.out.print(itemQuery.get("veManguera")+ " ");
+        	System.out.print(itemQuery.get("vePago")+ " ");
+        	System.out.print(itemQuery.get("veProducto")+ " "); 
+        	System.out.print(itemQuery.get("vePrecio")+ " ");
+        	System.out.print(itemQuery.get("vePago")+ " ");
+        	System.out.print(itemQuery.get("veVolumen")+ " "); 
+        	System.out.print(itemQuery.get("veImporte")+ " ");
+        	System.out.print(itemQuery.get("veEstado")+ " "); 
         	
-        	String mybody = "{\r\n"
+        	System.out.println(importeTh + " " + volumenTh);
+        	
+        	
+        	
+        	/*String mybody = "{\r\n"
     				+ "  \"DMG\": {\r\n"
     				+ "    \"F\": {\r\n"
-    				+ "      \"pf_user_id\": "+item.get("id")+",\r\n"
-    				+ "      \"nombreRecepcion\": \""+item.get("nombre")+"\",\r\n"
-    				+ "      \"fechaNacimientoRecepcion\": \""+fechaCast+"\",\r\n"
-    				+ "      \"asistenciaRecepcion\": 1\r\n"
+    				+ "      \"pf_user_id\": 1,\r\n"
+    				+ "      \"store_alias\": \"ovn_str_sac123\",\r\n"
+    				+ "      \"cat_type_pump_alias\": \"dmg_pumptype_r1\",\r\n"
+    				+ "      \"origin_id\": 4,\r\n"
+    				+ "      \"origin_datetime_pc\": \"08/05/2007 12:39:29\",\r\n"
+    				+ "      \"origin_datetime_cs\": \"08/05/2007 12:35:29\",\r\n"
+    				+ "      \"origin_side\": 165,\r\n"
+    				+ "      \"origin_hose\": 998,\r\n"
+    				+ "      \"origin_payment\": 567,\r\n"
+    				+ "      \"origin_price\": 165.087,\r\n"
+    				+ "      \"origin_volume\": 55.087,\r\n"
+    				+ "      \"origin_product\": \"hgk\",\r\n" 
+    				+ "      \"origin_amount\": 3456.987,\r\n"
+    				+ "      \"origin_tot_volume\": 8833.776,\r\n"
+    				+ "      \"origin_tot_amount\": 8661.020,\r\n"
+    				+ "      \"status\": 1\r\n"
     				+ "    }\r\n"
     				+ "  }\r\n"
     				+ "}";
     			
     		HttpResponse<String> responseT = 
-                    Unirest.post("http://localhost:8462/ovnDMG/module/dmg/DMGReceptionServiceImp/dmgreceptionRegister").body(mybody).asString();
+                    Unirest.post("http://localhost:8462/ovnDMG/module/dmg/DMGGasoline_PumpServiceImp/dmggasolinePumpRegister").body(mybody).asString();
     		
-    		System.out.println(responseT.getBody());
+    		System.out.println(responseT.getBody());*/
         	
         	
         	
-            Tabla tPruebaEdit = newJpo.tabla("pruebas");
+            /*Tabla tPruebaEdit = newJpo.tabla("pruebas");
             
              tPruebaEdit.donde("id ="+item.get("id"));
             
@@ -129,15 +185,14 @@ public class ScheduledTasks {
             
              tPruebaEdit.editar(); // UPDATE pruebas SET estado = 0 where pruea_id = 1*/
             
-        	
-        	
         }
+        	
+        
         
         newJpo.commit();
-        
-        System.out.println("Resultado de prueba");
+             
         System.out.println((new Gson()).toJson(resultadoaTotalizadorH));
-
+        System.out.println((new Gson()).toJson(resultadoQuery));
 		System.out.println("Hola mundo");
 	}
 }
